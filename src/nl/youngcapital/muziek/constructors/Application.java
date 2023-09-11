f6a424d71015b0dac2680179eb709d54214ae84a
Raymond Loman <r.loman@recognize.nl>
2023-09-11 14:28:37 +0200

    Add demo code for consructor and encapsulation

--- src/nl/youngcapital/muziek/constructors/Application.java
+++ src/nl/youngcapital/muziek/constructors/Application.java
@@ -6,6 +6,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		Person piet = new Person("Piet");
-		System.out.println(piet.name);
+		System.out.println(piet.getName());
+		
+		Person raymond = new Person("Raymond", 55);
+		
+		System.out.println(raymond.getAge());
+		System.out.println(raymond.getName());
+		
+		raymond.setAge(-3);
+		System.out.println(raymond.getAge());
 	}
 }
--- src/nl/youngcapital/muziek/model/Person.java
+++ src/nl/youngcapital/muziek/model/Person.java
@@ -2,8 +2,9 @@ package nl.youngcapital.muziek.model;
 
 public class Person {
 	
-	public String name;
-	public int age;
+	private String name;
+	private int age;
+	
 	
 	// Een verplichtingsregel in Java dwing je af met een constructor
 	// Syntax
@@ -15,5 +16,36 @@ public class Person {
 	 */
 	public Person(String name) {
 		this.name = name;
+	}
+	
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Person() {
+		
+	}
+
+
+	// met getters en setters implemeneer je: Encapsulation
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		if(age < 0) {
+			System.err.println("Leeftijd moet groter of gelijk aan 0 zijn");
+			return;
+		}
+		
+		this.age = age;
+	}
+
+
+	public String getName() {
+		return name;
 	}
 }
